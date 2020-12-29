b55fd2daa58b5f3611791461f96eb8e942603399
Kristian B <KrisZane@users.noreply.github.com>
2020-12-29 12:22:19 +0100

    [BUGF] Fixed a small bug with the gui lib

--- trellocompactor/index.user.js
+++ trellocompactor/index.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Trello Compactor
 // @namespace    http://tampermonkey.net/
-// @version      0.25
+// @version      0.26
 // @description  Compacts the Trello view and adds some more info.
 // @author       https://github.com/Nexination
 // @match        https://trello.com/b/*
@@ -13,64 +13,63 @@
 //'use strict';
 
 class TrelloCompactor {
-  constructor() {
-    if (GM_getValue('coordinates') === undefined) {
-      GM_setValue('coordinates', {"x": 25, "y": 700});
-    };
-    let coordinates = GM_getValue('coordinates');
+    constructor() {
+        if (GM_getValue('coordinates') === undefined) {
+            GM_setValue('coordinates', {"x": 25, "y": 700});
+        };
+        let coordinates = GM_getValue('coordinates');
 
-    this.microGui = new MicroGui(coordinates);
-    
-    let timer = setTimeout(() => {this.delayRun();}, 1500);
-  }
-  delayRun() {
-    this.addStyle();
-    this.indexer();
-  };
-  addStyle() {
-    let styleObject = {
-      ".js-badges .badge, .list-card-details .list-card-members": {
-        "display": "none"
-      }
-      , ".js-badges .is-due-future, .js-badges .is-due-now, .js-badges .is-due-soon, .js-badges .is-due-past, .js-badges .is-unread-notification": {
-        "display": "inline !important"
-      }
-      , ".js-badges .badge-text": {
-        "font-size": "0.7em"
-      }
-      ,".list-card-details .list-card-labels, .list-card-details .list-card-title": {
-        "display": "inline"
-        , "font-size": "0.9em"
-      }
-      ,
-    };
-    let styleComposite = '';
-    for(let i in styleObject) {
-      styleComposite += i + '{\n';
-      for(let j in styleObject[i]) {
-        styleComposite += j + ': ' + styleObject[i][j] + ';\n';
-      };
-      styleComposite += "}\n";
-    };
-    GM_addStyle(styleComposite);
-  }
-  indexer() {
-    let cardLists = document.getElementsByClassName('js-list-content');
-    let cardCountTotal = 0;
-    let uiList = [];
+        this.microGui = new MicroGui(coordinates);
 
-    for(let i = 0; i < cardLists.length; i += 1) {
-      let cardList = cardLists[i];
-      let cardCount = cardList.getElementsByClassName('list-cards')[0].children.length;
-      cardCountTotal += cardCount;
-
-      uiList.push({"type": "text", "value": cardList.getElementsByClassName('list-header-name')[0].value + " (" + cardCount + ")"});
+        let timer = setTimeout(() => {this.delayRun();}, 1500);
+    }
+    delayRun() {
+        this.addStyle();
+        this.indexer();
     };
-    uiList.push({"type": "text", "value": "Total: " + cardCountTotal});
-    this.microGui.clearGui();
-    this.microGui.createGui(uiList);
+    addStyle() {
+        let styleObject = {
+            ".js-badges .badge, .list-card-details .list-card-members": {
+                "display": "none"
+            }
+            , ".js-badges .is-due-future, .js-badges .is-due-now, .js-badges .is-due-soon, .js-badges .is-due-past, .js-badges .is-unread-notification": {
+                "display": "inline !important"
+            }
+            , ".js-badges .badge-text": {
+                "font-size": "0.7em"
+            }
+            ,".list-card-details .list-card-labels, .list-card-details .list-card-title": {
+                "display": "inline"
+                , "font-size": "0.9em"
+            }
+            ,
+        };
+        let styleComposite = '';
+        for(let i in styleObject) {
+            styleComposite += i + '{\n';
+            for(let j in styleObject[i]) {
+                styleComposite += j + ': ' + styleObject[i][j] + ';\n';
+            };
+            styleComposite += "}\n";
+        };
+        GM_addStyle(styleComposite);
+    }
+    indexer() {
+        let cardLists = document.getElementsByClassName('js-list-content');
+        let cardCountTotal = 0;
+        let uiList = [];
 
-    let timer = setTimeout(() => {this.indexer();}, 10000);
-  };
+        for(let i = 0; i < cardLists.length; i += 1) {
+            let cardList = cardLists[i];
+            let cardCount = cardList.getElementsByClassName('list-cards')[0].children.length;
+            cardCountTotal += cardCount;
+
+            uiList.push({"type": "text", "value": cardList.getElementsByClassName('list-header-name')[0].value + " (" + cardCount + ")"});
+        };
+        uiList.push({"type": "text", "value": "Total: " + cardCountTotal});
+        this.microGui.addGui(uiList);
+
+        let timer = setTimeout(() => {this.indexer();}, 10000);
+    };
 };
 let trelloCompactor = new TrelloCompactor();
